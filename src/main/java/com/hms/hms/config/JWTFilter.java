3071953b6b1a07288909f496745163e964f04876
Krishna-cse <krishnaofficialemail@gmail.com>
2024-10-24 19:06:28 +0530

    23-10-2024

---
+++ src/main/java/com/hms/hms/config/JWTFilter.java
@@ -0,0 +1,58 @@
+package com.hms.hms.config;
+
+import com.hms.hms.entity.AppUser;
+import com.hms.hms.repository.AppUserRepository;
+import com.hms.hms.service.JWTService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@Component
+public class JWTFilter extends OncePerRequestFilter {
+
+    private JWTService jwtService;
+    private AppUserRepository appUserRepository;
+
+    public JWTFilter(JWTService jwtService, AppUserRepository appUserRepository) {
+        this.jwtService = jwtService;
+        this.appUserRepository = appUserRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+         String token = request.getHeader("Authorization");
+        System.out.println(token);
+        if(token!=null && token.startsWith("Bearer ")){
+            String tokenVal = token.substring(7).trim();
+            String username = jwtService.getUsername(tokenVal);
+            System.out.println(username);
+            Optional<AppUser> opUsername = appUserRepository.findByUsername(username);
+            if(opUsername.isPresent()){
+                AppUser appUser = opUsername.get();
+                UsernamePasswordAuthenticationToken
+                        authenticationToken =
+                new UsernamePasswordAuthenticationToken(appUser,null,null);
+                authenticationToken.setDetails(new WebAuthenticationDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+
+            }
+
+
+        }
+        filterChain.doFilter(request,response);
+    }
+}
