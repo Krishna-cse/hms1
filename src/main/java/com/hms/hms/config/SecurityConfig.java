d0d3380a44f3184d5fa497cf5df53e6480d9d444
Krishna-cse <krishnaofficialemail@gmail.com>
2024-10-30 20:02:45 +0530

    29-10-2024

--- src/main/java/com/hms/hms/config/SecurityConfig.java
+++ src/main/java/com/hms/hms/config/SecurityConfig.java
@@ -23,12 +23,12 @@ public class SecurityConfig {
 
         http.addFilterBefore(jwtFilter, AuthorizationFilter.class);
         //haap
-       // http.authorizeHttpRequests().anyRequest().permitAll();
-        http.authorizeHttpRequests().
-                requestMatchers("/api/v1/users/login","/api/v1/users/signup","/api/v1/users/signup-property-owner")
-                .permitAll()
-                .requestMatchers("/api/v1/users/country/addCountry").hasAnyRole("OWNER","ADMIN")
-                .anyRequest().authenticated();
+        http.authorizeHttpRequests().anyRequest().permitAll();
+//        http.authorizeHttpRequests().
+//                requestMatchers("/api/v1/users/login","/api/v1/users/signup","/api/v1/users/signup-property-owner")
+//                .permitAll()
+//                .requestMatchers("/api/v1/users/country/addCountry").hasAnyRole("OWNER","ADMIN")
+//                .anyRequest().authenticated();
 
         return http.build();
     }
